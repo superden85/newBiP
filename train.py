473b14f5b3fc0c651748622d0f199faa63434aa6
Denis Duval <denis.duval13@gmail.com>
2023-06-22 13:43:28 +0200

    Fixed bug

--- train.py
+++ train.py
@@ -221,21 +221,8 @@ def main():
         clone_results_to_latest_subdir(
             result_sub_dir, os.path.join(result_main_dir, "latest_exp")
         )
-
-        #detailed log of the sparsity of the mask:
-        l0, l1, linf = 0, 0, 0
-        l001, l01, l05 = 0, 0, 0, 0
-        for (name, vec) in model.named_modules():
-            if hasattr(vec, "popup_scores"):
-                attr = getattr(vec, "popup_scores")
-                if attr is not None:
-                    l0 += torch.sum(attr != 0).item()
-                    l1 += abs(torch.sum(attr).item())
-                    linf = max(linf, abs(torch.max(attr).item()))
-                    l001 += torch.sum(abs(attr) < 0.01).item()
-                    l01 += torch.sum(abs(attr) < 0.1).item()
-                    l05 += torch.sum(abs(attr) < 0.5).item()
         
+        #detailed log of the sparsity of the model
         if args.trainer == "penalized_bilevel1":
             l0, l1, linf = 0, 0, 0
             l001, l01, l05 = 0, 0, 0
@@ -252,7 +239,7 @@ def main():
             logger.info(
                 f"Epoch {epoch}, l0 norm : {l0}, l1 norm : {l1}, linf norm: {linf}, \n Below 0.01: {l001}, Below 0.1: {l01}, Below 0.5: {l05}"
             )
-            
+
             print(f"Epoch {epoch}, l0 norm : {l0}, l1 norm : {l1}, linf norm: {linf}, \n Below 0.01: {l001}, Below 0.1: {l01}, Below 0.5: {l05}")
                     
 
