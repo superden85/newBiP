5b128547117b2bfdb14aab1dec72808d09b02119
Denis Duval <denis.duval13@gmail.com>
2023-06-30 14:22:40 +0200

    Add a script to compare masks

---
+++ superimpose.py
@@ -0,0 +1,70 @@
+import argparse
+
+def main():
+    # Create an ArgumentParser object
+    parser = argparse.ArgumentParser(description='Superimpose the repartition functions of the popup scores')
+
+    # Add command line arguments
+    parser.add_argument('--path', type=str, help='Text file with all the paths to the checkpoints')
+    parser.add_argument('--n', type=int, help='Number of points to plot', default=1000)
+    #parser.add_argument('--save-path', type=str, help='Path to the image of the plot')
+
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Access the argument values
+    file_path = args.arg1
+    n_points = args.arg2
+
+    checkpoint_label = []
+    # Open the text file for reading
+    with open('path', 'r') as file:
+        # Read the file line by line
+        for line in file:
+            # Split the line into two words
+            checkpoint, label = line.strip().split(' ')
+
+            checkpoint_label.append((checkpoint, label))
+
+    
+    common_length = None
+    x = linspace(0, mask_list[-1], n_points)
+    #plot the repartition functions of each mask on the same plot
+    for (checkpoint, label) in checkpoint_label:
+        checkpoint = load(path)
+        model_dict = checkpoint['state_dict']
+
+        mask_list = []
+        for (name, tensor) in model_dict.items():
+            if 'popup_scores' in name:
+                #retrieve the params of the layer
+                mask_list.extend(tensor.view(-1).detach().tolist())
+        
+        mask_length = len(mask_list)
+        if common_length is not None and mask_length != common_length:
+            raise ValueError(f'Length of mask for {checkpoint} is {mask_length} while it should be {common_length}.')
+        mask_list.sort()
+        
+
+        probs = zeros(n_points)
+        pointer = 0
+        for i in range(n_points):
+            while pointer < mask_length and mask_list[pointer] < x[i]:
+                pointer += 1
+            probs[i] = pointer / mask_length
+
+        plt.plot(x, probs)
+
+    # Save the plot
+    save_directory = 'plots'
+    os.makedirs(save_directory, exist_ok=True)  # Create the directory if it doesn't exist
+
+    save_path = os.path.join(save_directory, 'plot.png')
+    plt.savefig(save_path)
+
+    print('Plot saved.')
+
+
+if __name__ == '__main__':
+    main()
