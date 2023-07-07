9ba1ac23059cbb34c22be0f7192bda9257905972
Denis Duval <denis.duval13@gmail.com>
2023-07-07 16:54:01 +0200

    stats logging change

--- trainer/penalized_bilevel_1.py
+++ trainer/penalized_bilevel_1.py
@@ -194,7 +194,7 @@ def train(
         if i % args.print_freq == 0:
             progress.display(i)
         
-        if i <= 10:
+        if i == 0:
             #print stats
             l0, l1 = 0, 0
             mini, maxi = 1000, -1000
