cd5d4fd4a8b5ca78939058aa80e8a7da3ebcc1e5
Denis Duval <denis.duval13@gmail.com>
2023-07-08 16:45:12 +0200

    fixed error

--- utils/model.py
+++ utils/model.py
@@ -395,7 +395,7 @@ def get_epoch_data(model):
             attr = getattr(vec, "popup_scores")
             if attr is not None:
                 for i in range(0, 21):
-                    below_treshold[i] += torch.sum(attr < treshold_exp_list[i]).item() / total
+                    percentage_below_treshold[i] += torch.sum(attr < treshold_exp_list[i]).item() / total
     
     epoch_data.append((treshold_exp_list, below_treshold))
 
