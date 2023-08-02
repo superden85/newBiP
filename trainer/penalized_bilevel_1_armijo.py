1fff5695306721be298de3646662dbe10bb9dc66
Denis Duval <denis.duval13@gmail.com>
2023-08-02 15:21:33 +0200

    debug

--- trainer/penalized_bilevel_1_armijo.py
+++ trainer/penalized_bilevel_1_armijo.py
@@ -137,7 +137,7 @@ def train(
             print('-2.5-')
             mk_only = mask_tensor_only_inverse(model.parameters())
             print('Coefficient at 85 :', mk_only[85].item())
-            print('Intersection over Union :', calculate_IOU(mk_only_old, mk_only).item())
+            print('Number of different coefficients :', (mk_only_old != mk_only).sum().item())
             print('-2.5-')
 
             acc1, acc5 = accuracy(output, val_targets, topk=(1, 5))
