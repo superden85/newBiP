e2e5543b1c09ef3bfdfd6ec3b8e8bceea3eaf2a7
Denis Duval <denis.duval13@gmail.com>
2023-08-02 12:10:31 +0200

    debug

--- trainer/penalized_bilevel_1_armijo.py
+++ trainer/penalized_bilevel_1_armijo.py
@@ -202,7 +202,6 @@ def train(
                 print('USH')
 
 
-
             #here we are doing the armijo line search for the stepsize
 
             step_size = 1.0
@@ -244,6 +243,9 @@ def train(
             if i <= 10:
                 #print the number of steps in the armijo line search
                 print("number of steps in the line search: ", counter)
+                if counter == 0:
+                    mk = mask_tensor(model.parameters())
+                    print("Number of different values in mk and mstar : ", torch.sum(mk != m_star).item())
 
 
         batch_time.update(time.time() - end)
