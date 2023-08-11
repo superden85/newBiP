8d696408eab1c74c4e707ae758e2fa986d6b3e85
Denis Duval <denis.duval13@gmail.com>
2023-08-11 23:24:06 +0200

    debug

--- trainer/penalized_bilevel_1_corrected.py
+++ trainer/penalized_bilevel_1_corrected.py
@@ -145,7 +145,7 @@ def train(
                     if hasattr(vec, "weight"):
                         attr = getattr(vec, "weight")
                         if attr is not None:
-                            vec.w = param_list[pointer:pointer + attr.numel()].view_as(attr)
+                            attr.data = param_list[pointer:pointer + attr.numel()].view_as(attr)
                             pointer += attr.numel()
             
             for (name, param) in dummy_model.named_parameters():
