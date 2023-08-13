a7fb882840644ecb8496c0b3aab54af13be09169
Denis Duval <denis.duval13@gmail.com>
2023-08-13 16:57:43 +0200

    debug

--- trainer/penalized_bilevel_1_corrected.py
+++ trainer/penalized_bilevel_1_corrected.py
@@ -169,7 +169,9 @@ def train(
                         torch.norm(first_part[big_pointer:big_pointer+numel] - param_list[small_pointer:small_pointer + numel] * grad_z_list[small_pointer:small_pointer + numel], p=0))
                         small_pointer += numel
                     big_pointer += numel
-            
+                print(small_pointer, big_pointer)
+                print(param_list.shape, grad_z_list.shape, first_part.shape)
+                
             score_list = torch.cat([score.view(-1) for score in score_list])
             implicit_gradient = -args.lr2 * score_list * grad_z_list ** 2
 
