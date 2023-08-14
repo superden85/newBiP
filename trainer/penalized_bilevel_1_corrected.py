ae2f28c923e819a9a09ac04cb7437f4da2a678c8
Denis Duval <denis.duval13@gmail.com>
2023-08-14 18:45:26 +0200

    debug

--- trainer/penalized_bilevel_1_corrected.py
+++ trainer/penalized_bilevel_1_corrected.py
@@ -236,7 +236,7 @@ def train(
 
                     if param.requires_grad:
                         #(1 - step_size) * param.data + step_size * m_star[pointer:pointer + num_param].view_as(param).data
-                        param.data.copy_(torch.zeros_like(param.data))
+                        param.data.copy_(torch.ones_like(param.data))
 
                     pointer += num_param
             
