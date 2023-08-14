0eda6a6b0333f1762ad15186b8f3783810ff7ca4
Denis Duval <denis.duval13@gmail.com>
2023-08-14 18:27:55 +0200

    debug

--- trainer/penalized_bilevel_1_corrected.py
+++ trainer/penalized_bilevel_1_corrected.py
@@ -226,7 +226,7 @@ def train(
             step_size = 2/(epoch * len(train_loader) + i + 2)
 
             #then we update the parameters
-            """ with torch.no_grad():
+            with torch.no_grad():
                 pointer = 0
                 for param in model.parameters():
                     num_param = param.numel()
@@ -235,13 +235,9 @@ def train(
                     #i.e. if param.requires_grad = True
 
                     if param.requires_grad:
-                        param.data.copy_((1 - step_size) * param.data + step_size * m_star[pointer:pointer + num_param].view_as(param).data)
+                        param.data.copy_((1 - step_size) * param.data + step_size * m_star[pointer:pointer + num_param].view_as(param))
 
-                    pointer += num_param """
-            
-            for m in model.modules():
-                if hasattr(m, 'popup_scores'):
-                    nn.init.normal_(m.popup_scores, mean=0, std=0.01)
+                    pointer += num_param
 
             #we want to compute the duality gap as well
             #it is equal to d = - <outer_gradient, m_star - params>
