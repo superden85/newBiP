6ec86741906b793ac35929642ea25532ce998eef
Denis Duval <denis.duval13@gmail.com>
2023-08-20 01:51:08 +0200

    debug

--- trainer/penalized_bilevel_1_corrected.py
+++ trainer/penalized_bilevel_1_corrected.py
@@ -215,7 +215,7 @@ def train(
         if i % args.print_freq == 0:
             progress.display(i)
         
-        if i >= 0:
+        if i <= 3:
             #print stats
             l0, l1 = 0, 0
             mini, maxi = 1000, -1000
