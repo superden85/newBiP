1ea3687884b66ed2b80031ec5637bccb48b01506
Denis Duval <denis.duval13@gmail.com>
2023-09-19 14:56:05 +0200

    added a fmnist_model

--- models/basic.py
+++ models/basic.py
@@ -116,28 +116,7 @@ class MnistModel(nn.Module):
     
     def forward(self, x):
         return self._forward_impl(x)
-    
-    def get_mask(self):
-        mask = []
-        for (name, vec) in self.named_modules():
-            if hasattr(vec, "popup_scores"):
-                attr = getattr(vec, "popup_scores")
-                if attr is not None:
-                    mask.append(attr.view(-1).detach())
-        return torch.cat(mask)
-    
-"""     def prune(self):
-        #set the weights that are zero in the mask to zero weights in the model
-        #and set an attribute frozen = True for them
 
-        for (name, vec) in self.named_modules():
-            if hasattr(vec, "popup_scores"):
-                attr = getattr(vec, "popup_scores")
-                if attr is not None:
-                    mask = attr.view(-1).detach()
-                    vec.weight.data *= mask
-                    vec.frozen = True """
-        
 class MnistModelMini(nn.Module):
 
     def __init__(self, conv_layer, linear_layer, init_type='kaiming_normal', **kwargs):
@@ -185,6 +164,63 @@ class MnistModelMini(nn.Module):
     def forward(self, x):
         return self._forward_impl(x)
 
+class FashionMnistModel(nn.Module):
+
+    def __init__(self, conv_layer, linear_layer, init_type='kaiming_normal', **kwargs):
+        super(MnistModel, self).__init__()
+        self.conv1 = conv_layer(1, 48, 4, stride=2, padding=1)  # Increase output channels to 48
+        self.conv2 = conv_layer(48, 96, 4, stride=2, padding=1)  # Increase output channels to 96
+        self.fc1 = linear_layer(96 * 7 * 7, 300)  # Increase the number of neurons in fc1 to 300
+        self.fc2 = linear_layer(300, 10)  # Keep fc2 as it is
+
+        self.num_classes = kwargs['num_classes'] if 'num_classes' in kwargs else 10
+        self.k = kwargs['k'] if 'k' in kwargs else None
+        self.unstructured_pruning = kwargs['unstructured'] if 'unstructured' in kwargs else False
+
+    def _forward_impl(self, x):
+        if self.unstructured_pruning:
+            score_list = []
+            for (name, vec) in self.named_modules():
+                if hasattr(vec, "popup_scores"):
+                    attr = getattr(vec, "popup_scores")
+                    if attr is not None:
+                        score_list.append(attr.view(-1))
+            scores = torch.cat(score_list)
+            adj = GetSubnetUnstructured.apply(scores.abs(), self.k)
+
+            pointer = 0
+            for (name, vec) in self.named_modules():
+                if not isinstance(vec, (nn.BatchNorm2d, nn.BatchNorm2d)):
+                    if hasattr(vec, "weight"):
+                        attr = getattr(vec, "weight")
+                        if attr is not None:
+                            numel = attr.numel()
+                            vec.w = attr * adj[pointer: pointer + numel].view_as(attr)
+                            pointer += numel
+        else:
+            pointer = 0
+            for (name, vec) in self.named_modules():
+                if not isinstance(vec, (nn.BatchNorm2d, nn.BatchNorm2d)):
+                    if hasattr(vec, "weight"):
+                        attr = getattr(vec, "weight")
+                        if attr is not None:
+                            vec.w = attr
+                            pointer += attr.numel()
+        
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = Flatten()(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+
+        return x
+    
+    def forward(self, x):
+        return self._forward_impl(x)
+
 def mnist_model(conv_layer, linear_layer, init_type='kaiming_normal', **kwargs):
     assert init_type == "kaiming_normal", "only supporting kaiming_normal init"
     model = MnistModel(conv_layer, linear_layer, init_type, **kwargs)
@@ -193,6 +229,11 @@ def mnist_model(conv_layer, linear_layer, init_type='kaiming_normal', **kwargs):
 def mnist_model_mini(conv_layer, linear_layer, init_type='kaiming_normal', **kwargs):
     assert init_type == "kaiming_normal", "only supporting kaiming_normal init"
     model = MnistModelMini(conv_layer, linear_layer, init_type, **kwargs)
+    return model
+
+def fmnist_model(conv_layer, linear_layer, init_type='kaiming_normal', **kwargs):
+    assert init_type == "kaiming_normal", "only supporting kaiming_normal init"
+    model = FashionMnistModel(conv_layer, linear_layer, init_type, **kwargs)
     return model
     
 def mnist_model_large(conv_layer, linear_layer, init_type, **kwargs):
