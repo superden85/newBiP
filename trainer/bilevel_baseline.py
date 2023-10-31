9f853ed602485ce559ef950f63413362ff809780
Denis Duval <denis.duval13@gmail.com>
2023-10-31 08:31:53 +0100

    toy

--- trainer/bilevel_baseline.py
+++ trainer/bilevel_baseline.py
@@ -101,14 +101,6 @@ def train(
             mask_grad_vec = grad2vec(model.parameters())
 
             implicit_gradient = -args.lr2 * mask_grad_vec * param_grad_vec
-
-            """ if i == 2:
-                pointer = 0
-                for param in model.parameters():
-                    print(param.shape, param.requires_grad)
-                    num_param = param.numel()
-                    print("Mask grad vec and Param grad vec are equal: ", torch.all(mask_grad_vec[pointer: pointer + num_param] == param_grad_vec[pointer: pointer + num_param]))
-                    pointer += num_param """
             
             def append_grad_to_vec(vec, parameters):
 
